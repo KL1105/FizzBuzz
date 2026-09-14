2c21f0c04034e60e7a260f415f7ef840fbdd89f0
Kobi Liu <kliu0511@outlook.com>
2026-09-14 14:18:58 -0400

    Main added and public function doFizzBuzz for access in Main

--- src/lab1/Main.java
+++ src/lab1/Main.java
@@ -2,7 +2,9 @@ package lab1;
 
 public class Main {
     public static void main(String[] args) {
-        FizzBuzz.doFizzBuzz(5);
+        //FizzBuzz.doFizzBuzz(5);
+
+        FizzBuzz.main(new String[]{});;
     }
 }
 
